dcac59262a8b8c5cd6c4d6a371a802a20debbf06
Julien Delhaye <julien@delhaye.me>
2019-03-19 19:29:33 +0100

    fixed some python 2 to python3 str issues

--- domaintools.py
+++ domaintools.py
@@ -4,7 +4,6 @@
 from flask import Flask, request, url_for, render_template
 import socket
 import subprocess
-import dns.resolver
 import whois_helper
 
 app = Flask(__name__)
@@ -12,7 +11,7 @@ app.config.from_object(__name__)
 app.debug = True
 
 def whois(domain):
-    tld = domain.split(".")[-1]
+    tld = domain.split(b'.')[-1]
     if tld == "com" or tld == "net" or tld == "org" or tld == "info":
         result = whois_helper.gtld(domain)
     elif tld == "fr" or tld == "re":
@@ -44,7 +43,7 @@ def digger(dom, trace=False, field='A', exchange=True):
             res['discover'] = dig_autodiscover_res
         dig_a_process = subprocess.Popen(["dig", dom, "+short"], stdout=subprocess.PIPE)
         dig_a_res = dig_a_process.communicate()[0][:-1]
-        if "\n" in dig_a_res: #multiples A fields
+        if "\n" in str(dig_a_res): #multiples A fields
             dig_a_res = " - ".join(dig_a_res.split("\n"))
         if isinstance(dig_a_res, list):
             try:
@@ -58,13 +57,13 @@ def digger(dom, trace=False, field='A', exchange=True):
                 host = u"impossible de récupérer le nom d'hôte"
 
         dig_ns_process = subprocess.Popen(["dig", dom, "NS", "+short"], stdout=subprocess.PIPE)
-        dig_ns_res = dig_ns_process.communicate()[0][:-1]
+        dig_ns_res = str(dig_ns_process.communicate()[0][:-1])
 
         dig_mx_process = subprocess.Popen(["dig", dom, "MX", "+short"], stdout=subprocess.PIPE)
-        dig_mx_res = dig_mx_process.communicate()[0][:-1]
+        dig_mx_res = str(dig_mx_process.communicate()[0][:-1])
 
         dig_txt_process = subprocess.Popen(["dig", dom, "TXT", "+short"], stdout=subprocess.PIPE)
-        dig_txt_res = dig_txt_process.communicate()[0][:-1]
+        dig_txt_res = str(dig_txt_process.communicate()[0][:-1])
 
         res['A'] = dig_a_res
         res['HOST'] = host
@@ -84,35 +83,7 @@ def dig_on_server(dom, serv):
         res = "Le domaine n'est pas déclaré sur le serveur."
     return res
 
-def rbl_check(ip):
-    bls = ["zen.spamhaus.org",
-            "spam.abuse.ch",
-            "cbl.abuseat.org",
-            "virbl.dnsbl.bit.nl",
-            "dnsbl.inps.de",
-            "ix.dnsbl.manitu.net",
-            "dnsbl.sorbs.net",
-            "bl.spamcannibal.org",
-            "bl.spamcop.net",
-            "xbl.spamhaus.org",
-            "pbl.spamhaus.org",
-            "dnsbl-1.uceprotect.net",
-            "dnsbl-2.uceprotect.net",
-            "dnsbl-3.uceprotect.net",
-            "db.wpbl.info"]
-    confirmed_bl = []
-    not_bl = []
-    for bl in bls:
-        try:
-            my_resolver = dns.resolver.Resolver()
-            query = '.'.join(reversed(str(ip).split("."))) + "." + bl
-            answers = my_resolver.query(query, "A")
-            answer_txt = my_resolver.query(query, "TXT")
-            ans = '{0} - {1}'.format(bl, answer_txt[0])
-            confirmed_bl.append(ans)
-        except dns.resolver.NXDOMAIN:
-            not_bl.append(bl)
-    return confirmed_bl, not_bl
+
 
 @app.route("/")
 def index():
@@ -147,47 +118,6 @@ def whois_dig():
         whois_dom = whois(domain)
     return render_template('whois-dig.html', domain=domain, whois=whois_dom, dig_to=dig_to, res=dig_result, trace=trace, serv=serveur)
 
-@app.route('/propadns', methods=['GET', 'POST'])
-def propadns():
-    context = {}
-    res=[]
-    domain=""
-    if request.method == 'POST':
-        context['res'] = []
-        domain = request.form['domain'].strip().encode('idna')
-        context['domain'] = domain
-        servers = [
-            {"ip":"8.8.8.8", "localisation":"google (US)"},
-            {"ip":"208.67.222.222", "localisation":"openDNS (US)"},
-            {"ip":"8.26.56.26", "localisation":"comodo (US)"},
-            {"ip":"156.154.70.1", "localisation":"DNS advantage (US)"},
-            {"ip":"198.153.192.50", "localisation":"norton DNS (US)"},
-            {"ip":"109.69.8.51", "localisation":"puntCAT (espagne)"},
-            {"ip":"89.233.43.71", "localisation":"censurfridns.dk (danemark) "},
-            {"ip":"37.235.1.174", "localisation":"FreeDNS (autriche)"},
-            {"ip":"195.46.39.39", "localisation":"SafeDNS (Russie)"},
-            {"ip":"81.218.119.11", "localisation":"GreenTeamDNS (israel)"},
-            {"ip":"84.200.69.80", "localisation":"DNS.WATCH (Allemagne)"},
-            {"ip":"187.115.169.179","localisation":"Bresil"},
-            {"ip":"211.22.80.180","localisation":"taiwan"},
-        ]
-        for server in servers:
-            if "ns_check" in request.form.keys():
-                p = subprocess.Popen(["dig", domain, "NS", "@{}".format(server["ip"]), "+short"], stdout=subprocess.PIPE)
-                result =  p.communicate()[0][:-1]
-                ns = result.split("\n")
-                nss = sorted(ns)
-                result = " - ".join(nss)
-                res.append("servers : {1} at {0} ".format(server["localisation"], result))
-            else :
-                p = subprocess.Popen(["dig", domain, "@{}".format(server["ip"]), "+short"], stdout=subprocess.PIPE)
-                result =  p.communicate()[0][:-1]
-                res.append("servers : {1} at {0} ".format(server["localisation"], result))
-
-    return render_template('propadns.html', res=res, domain=domain)
-
-
-
 @app.route('/sortdom', methods=['GET', 'POST'])
 def sortdom():
     sorted_doms = {}
@@ -201,7 +131,7 @@ def sortdom():
             if "." not in domain:
                 #pas de point = pas de domaine = osef
                 continue
-            domain = domain.strip().encode('idna')
+            domain = str(domain.strip().encode('idna'))
             domains_list.append(domain)
             sorted_doms['.'.join(domain.split('.')[1:]).strip()] = []
         for tld in sorted_doms.keys():
